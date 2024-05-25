2007a2a63d544c829a2e1c1e328aad6b8f776a33
vncnttan <vincent.tanjaya.vt@gmail.com>
2024-05-25 12:24:03 +0700

    feat: UI

---
+++ frontend/src/components/TryAgainButton.jsx
@@ -0,0 +1,35 @@
+export default function TryAgainButton() {
+  // class="ms-3 mb-2 md:mb-0 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+  return (
+    <button
+      data-tooltip-target="tooltip-bottom"
+      data-tooltip-placement="bottom"
+      type="button"
+      className="relative duration-100 p-4 mt-4 flex flex-row gap-2 text-white w-fit mx-auto 
+      hover:bg-white hover:bg-opacity-10 rounded-md cursor-pointer"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="size-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 48.678 48.678 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 0 0 3.7 3.7 48.656 48.656 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3-3 3"
+        />
+      </svg>
+      <div
+        id="tooltip-bottom"
+        role="tooltip"
+        className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+      >
+        Tooltip on bottom
+        <div className="tooltip-arrow" data-popper-arrow></div>
+      </div>
+    </button>
+  );
+}
--- frontend/src/pages/LearningPage.jsx
+++ frontend/src/pages/LearningPage.jsx
@@ -1,11 +1,11 @@
-import  { useState } from "react";
+import { useState } from "react";
 import PageLayout from "../layouts/PageLayout.jsx";
 import image1 from "../images/image1.png"; // testing purposes
 import TranslationComponent from "../components/TranslationComponent.jsx";
 import PronounciationComponent from "../components/PronounciationComponent.jsx";
+import TryAgainButton from "../components/TryAgainButton.jsx";
 
 function LearningPage() {
-
   const [question, setQuestion] = useState({
     // Sample data, fetch from backend
     image: image1,
@@ -13,14 +13,15 @@ function LearningPage() {
     translation: "Hello, my name is Ben.",
   });
 
-  
-
   return (
     <PageLayout>
-        <div className="flex flex-col gap-2 h-screen py-40">
-            <TranslationComponent question={question} />
-            <PronounciationComponent question={question} />
+      <div className="flex flex-col h-screen py-32">
+        <div className="flex flex-col justify-evenly gap-4 grow ">
+          <TranslationComponent question={question} />
+          <PronounciationComponent question={question} />
         </div>
+        <TryAgainButton />
+      </div>
     </PageLayout>
   );
 }
